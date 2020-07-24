9c5fa8ec107b05846dd71a7dfe8fd1c1b1493edd
mbruty <48282784+BigBoiBruty@users.noreply.github.com>
2020-07-24 21:31:56 +0100

    :sparkles: Added start and settings modal

--- src/Desktop/Components/StartBar.js
+++ src/Desktop/Components/StartBar.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react'
-import DateTime from './DateTime';
+import React, { Component } from 'react';
+import StartModal from './Modals/StartModal';
+import DateTime from './DateTime';;
 export default class StartBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showStartModal: false
+        }
+        this.toggleStartModal = this.toggleStartModal.bind(this);
+    }
+
+    toggleStartModal(){
+        this.setState({...this.state, showStartModal: !this.state.showStartModal});
+    }
+
     render() {
         return (
-            <div class="taskbar">
-                <div class="icons">
-                    <div class="icons-left">
-                        <a id="start-menu"><i class="material-icons">widgets</i></a>
-                        <div className="search">
-                            <input type="text" placeholder="Type here to search"/>
-                            <i className="material-icons">search</i>
+            <div>
+                <StartModal show={this.state.showStartModal} toggle={this.toggleStartModal}/>
+                <div class="taskbar">
+                    <div class="icons">
+                        <div class="icons-left">
+                            <a id="start-menu" onClick={this.toggleStartModal}><i class="material-icons noselect clickthrough">widgets</i></a>
+                            <div className="search">
+                                <input type="text" placeholder="Type here to search"/>
+                                <i className="material-icons noselect">search</i>
+                            </div>
                         </div>
+                        <div className="icons-middle">
+                        </div>
+                        <DateTime/>
                     </div>
-                    <div className="icons-middle">
-                    </div>
-                    <DateTime/>
                 </div>
             </div>
         )
