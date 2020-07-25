916f0b7bf163616a09966ac2ee7dac35f01c4573
mbruty <48282784+BigBoiBruty@users.noreply.github.com>
2020-07-25 14:02:54 +0100

    :sparkles: MarkDown parsing within show project

---
+++ src/Desktop/Components/ShowProject.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import data from '../other-projects.json';
+export default class ShowProject extends Component {
+    constructor(props) {
+        super(props);
+        this.project = data.filter(x => x.name === this.props.name)[0];
+        this.renderMD = this.renderMD.bind(this);
+    }
+    renderMD(){
+        const data = this.project.info.split('\n');
+        let res = data.map(element => {
+            if(element.startsWith('####')){
+                element = <h4>{element.replace('####','')}</h4>;
+            }
+            else if(element.startsWith('###')){
+                element = <h3>{element.replace('###','')}</h3>;
+            }
+            else if(element.startsWith('##')){
+                element = <h2>{element.replace('##','')}</h2>;
+            }
+            else if(element.startsWith('#')){
+                element = <h1>{element.replace('#','')}</h1>;
+            }
+            else if(element.startsWith('-')){
+                element = <ul><li>{element.replace('-','')}</li></ul>
+            }
+            return <p>{element}</p>
+        })
+        console.log(res);
+        return res;
+    }
+    render() {
+        return (
+            <div className="project-container noselect" style={{height: this.props.height - 40, width: this.props.width}}>
+                <h1>{this.project.name}</h1>
+                <button onClick={() => {window.open(this.project.repo, "_blank")}}>View Code</button>
+                <h3>Completed project:</h3>
+                <img src={this.project.image} style={{width: this.props.width - 100}} />
+                <h3>Project info:</h3>
+                <div className="text-container">{this.renderMD()}</div>
+            </div>
+        )
+    }
+}
