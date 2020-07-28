f7299e639f8427d80692d095693c53912aa18043
mbruty <michael.bruty@students.plymouth.ac.uk>
2020-07-28 14:47:28 +0100

    :sparkles: :iphone: Started mobile version

---
+++ src/Mobile/Components/BottomBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BottomBar extends Component {
+    render() {
+        return (
+            <div className="bottom-bar">
+                <i className="material-icons up-carrot">keyboard_arrow_up</i>
+                
+            </div>
+        )
+    }
+}
--- src/Mobile/MobileLayout.js
+++ src/Mobile/MobileLayout.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import BottomBar from './Components/BottomBar';
+import './mobile.css';
+import '../material.css';
 export default class MobileLayout extends Component {
+
+    componentDidMount(){
+        document.title = "Android Brut-berry Pie";
+    }
     render() {
         return (
-            <div>
-                Mobile
+            <div className="mobile-container">
+                <div className="mobile-base">
+                <BottomBar/>
+                </div>
             </div>
         )
     }
