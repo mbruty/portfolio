22be3c57c8e0e0def2be17c90fdf1968604c6433
mbruty <michael.bruty@students.plymouth.ac.uk>
2020-07-29 13:23:53 +0100

    :sparkles: :art:  Added bottom bar

--- src/Mobile/Components/BottomBar.js
+++ src/Mobile/Components/BottomBar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import projects from '../../projects.json';
 export default class BottomBar extends Component {
     render() {
         return (
             <div className="bottom-bar">
-                <i className="material-icons up-carrot">keyboard_arrow_up</i>
-                
+                <i className="material-icons up-carrot noselect">keyboard_arrow_up</i>
+                <span className="icon-container">
+                    <i className="material-icons">account_circle</i>
+                    <i className="material-icons">account_circle</i>
+                    <i className="material-icons">account_circle</i>
+                    <i className="material-icons">account_circle</i>
+                </span>
             </div>
         )
     }
