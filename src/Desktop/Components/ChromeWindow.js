f6a7e6fcd164f568f9bd477027ed4ad3e6a7a2ba
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-10-12 15:26:53 +0100

    Refactoring and adding a 'show code' and 'show read me' section

--- src/Desktop/Components/ChromeWindow.js
+++ src/Desktop/Components/ChromeWindow.js
@@ -1,125 +1,284 @@
-import React, { Component } from 'react';
-import Draggable from 'react-draggable';
-import SortingVis from '../../Projects/Sorting Visualiser/App';
-import UrlShortener from '../../Projects/URL Shortener/App';
-import ReactResizeDetector from 'react-resize-detector';
-import Documents from './Documents';
-import ShowProject from '../../Global Components/ShowProject';
-import Contact from '../../Global Components/Contact';
+import React, { Component } from "react";
+import Draggable from "react-draggable";
+import SortingVis from "../../Projects/Sorting Visualiser/App";
+import UrlShortener from "../../Projects/URL Shortener/App";
+import ReactResizeDetector from "react-resize-detector";
+import Documents from "./Documents";
+import ShowProject from "../../Global Components/ShowProject";
+import Contact from "../../Global Components/Contact";
+import projects from "../../projects.json";
+import otherProjects from "../../other-projects.json";
 export default class ChromeWindow extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fullscreen: false,
-            fullscreenIcon: 'fullscreen',
-            grabbingCursor: 'grab',
-            width: 700,
-            height: 1000,
-        }
-        this.mouseDown = this.mouseDown.bind(this);
-        this.mouseUp = this.mouseUp.bind(this);
-        this.handleResize = this.handleResize.bind(this);
-        this.close = this.close.bind(this);
-        this.maximize = this.maximize.bind(this);
-        this.minimize = this.minimize.bind(this);
-        this.bringToFront = this.bringToFront.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullscreen: false,
+      fullscreenIcon: "fullscreen",
+      grabbingCursor: "grab",
+      width: 700,
+      height: 1000,
+      readMe: false,
+    };
+    this.mouseDown = this.mouseDown.bind(this);
+    this.mouseUp = this.mouseUp.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+    this.close = this.close.bind(this);
+    this.maximize = this.maximize.bind(this);
+    this.minimize = this.minimize.bind(this);
+    this.bringToFront = this.bringToFront.bind(this);
+  }
 
-    mouseDown(e){
-        //Bring to front
-        this.bringToFront();
-        this.setState({...this.state, grabbingCursor: 'grabbing'});
+  mouseDown(e) {
+    //Bring to front
+    this.bringToFront();
+    this.setState({ ...this.state, grabbingCursor: "grabbing" });
+  }
+  mouseUp() {
+    this.setState({ ...this.state, grabbingCursor: "grab" });
+  }
+  handleResize(w, h) {
+    this.setState({ ...this.state, width: w, height: h });
+  }
+  close() {
+    this.props.closeChrome(this.props.target);
+  }
+  maximize() {
+    if (this.state.fullscreen)
+      this.setState({
+        ...this.state,
+        fullscreen: false,
+        fullscreenIcon: "fullscreen",
+      });
+    else
+      this.setState({
+        ...this.state,
+        fullscreen: true,
+        fullscreenIcon: "fullscreen_exit",
+        width: window.innerWidth,
+        height: window.innerHeight - 43,
+      });
+  }
+  minimize() {
+    this.props.minimize(this.props.target);
+  }
+  bringToFront() {
+    this.props.bringToFront({ id: this.props.target, z: this.props.z });
+  }
+  toggleReadme = () => {
+    this.setState({ ...this.state, readMe: !this.state.readMe });
+  };
+  RenderProject = (name) => {
+    return (
+      <div>
+        <ShowProject
+          width={this.state.width}
+          height={this.state.height}
+          name={name.name}
+        />
+      </div>
+    );
+  };
+  renderContent(toRender) {
+    if (this.state.readMe) {
+      switch (toRender) {
+        case "Sorting Vis":
+          return <this.RenderProject name="Sorting Algorithm Visualiser" />;
+        case "URL Shortener":
+          return <this.RenderProject name="URL Shortener" />;
+        case "HUE Says":
+          return <this.RenderProject name="HUE Says" />;
+        default:
+          return null;
+      }
     }
-    mouseUp(){
-        this.setState({...this.state, grabbingCursor: 'grab'});
+    switch (toRender) {
+      case "Sorting Vis":
+        return (
+          <div id="sorting-vis">
+            <SortingVis width={this.state.width} height={this.state.height} />
+          </div>
+        );
+      case "URL Shortener":
+        return (
+          <div id="url-shortener">
+            <UrlShortener width={this.state.width} height={this.state.height} />
+          </div>
+        );
+      case "AirBnb Visualisation":
+        return (
+          <div id="air-bnb">
+            <ShowProject
+              width={this.state.width}
+              height={this.state.height}
+              name="AirBnb Visualisation"
+            />
+          </div>
+        );
+      case "WinForms Social Network":
+        return (
+          <div id="wf-social">
+            <ShowProject
+              width={this.state.width}
+              height={this.state.height}
+              name="WinForms Social Network"
+            />
+          </div>
+        );
+      case "HUE Says":
+        return (
+          <div
+            className="hue-says-container"
+            style={{ height: this.state.height - 40, width: this.state.width }}
+          >
+            <iframe
+              src={window.location.href + "game"}
+              style={{
+                height: this.state.height - 42,
+                width: this.state.width - 5,
+              }}
+            />
+          </div>
+        );
+      case "Recursion Example":
+        return (
+          <div
+            className="hue-says-container"
+            style={{ height: this.state.height - 40, width: this.state.width }}
+          >
+            <iframe
+              src={window.location.href + " "}
+              style={{
+                height: this.state.height - 42,
+                width: this.state.width - 4,
+              }}
+            />
+          </div>
+        );
+      case "Documents":
+        return (
+          <Documents
+            callback={this.props.showWindow}
+            width={this.state.width}
+            height={this.state.height}
+          />
+        );
+      case "Contact Me":
+        return <Contact width={this.state.width} height={this.state.height} />;
+      default:
+        break;
     }
-    handleResize(w, h) {
-        this.setState({...this.state, width: w, height: h});
-    }
-    close(){
-        this.props.closeChrome(this.props.target);
-    }
-    maximize() {
-        if(this.state.fullscreen) this.setState({...this.state, fullscreen: false, fullscreenIcon: 'fullscreen'});
-        else this.setState({...this.state, fullscreen: true, fullscreenIcon: 'fullscreen_exit',
-            width: window.innerWidth, height: window.innerHeight - 43});
-    }
-    minimize() {
-        this.props.minimize(this.props.target);
-    }
-    bringToFront(){
-        this.props.bringToFront({id: this.props.target, z: this.props.z})
-    }
-    renderContent(toRender) {
-        switch(toRender){
-            case 'Sorting Vis':
-                return (<div id="sorting-vis"><SortingVis width={this.state.width} height={this.state.height}/></div>)
-            case 'URL Shortener':
-                return (<div id="url-shortener"><UrlShortener width={this.state.width} height={this.state.height}/></div>)
-            case 'AirBnb Visualisation':
-                return(<div id="air-bnb"><ShowProject width={this.state.width} height={this.state.height} name="AirBnb Visualisation"/></div>)
-            case 'WinForms Social Network':
-                return (<div id="wf-social"><ShowProject width={this.state.width} height={this.state.height} name="WinForms Social Network"/></div>)
-            case 'HUE Says':
-                return (
-                <div className="hue-says-container" style={{height: this.state.height - 40, width: this.state.width}}>
-                    <iframe src={window.location.href+"game"} style={{height: this.state.height - 42,width: this.state.width - 5}}/>
-                </div>)
-            case 'Recursion Example':
-                return (
-                    <div className="hue-says-container" style={{height: this.state.height - 40, width: this.state.width}}>
-                    <iframe src={window.location.href+" "} style={{height: this.state.height - 42,width: this.state.width - 4}}/>
-                </div>)
-            case 'Documents':
-                return (<Documents callback={this.props.showWindow} width={this.state.width} height={this.state.height}/>)
-            case 'Contact Me':
-                return (<Contact width={this.state.width} height={this.state.height}/>);
-            default:
-                break;
-        }
-    }
-    render() {
-        if(!this.props.show) return null;
-        else if (!this.state.fullscreen) {
-            return (
-                    <ReactResizeDetector handleWidth handleHeight onResize={this.handleResize}>
-                        <Draggable
-                            handle=".handle"
-                            bounds="parent"
-                            defaultPosition={{x: 0, y: 0}}
-                            position={null}
-                            grid={[1, 1]}
-                            scale={1}>
-                        <div className="chrome-box" style={{zIndex: this.props.z}} onClick={this.bringToFront}>
-                            <div id="handle" className="handle" style={{cursor: this.state.grabbingCursor}}
-                                onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>
-                                    <h3>{this.props.target}</h3>
-                                    <div className="chrome-x"><i className="material-icons" onClick={this.close}>close</i></div>
-                                    <div className="chrome-btn"><i className="material-icons" onClick={this.maximize}>{this.state.fullscreenIcon}</i></div>
-                                    <div className="chrome-btn"><i className="material-icons" onClick={this.minimize}>minimize</i></div>
-                                </div>
-                            <div style={{overflow: 'auto'}}>
-                                {this.renderContent(this.props.target)}
-                            </div>
-                        </div>
-                    </Draggable>
-                </ReactResizeDetector>
-            )
-        }
-        else{
-            return(
-                <div style={{zIndex: 999}}>
-                        <div id="handle" className="handle" style={{cursor: 'auto'}}>
-                            <h3>{this.props.target}</h3>
-                            <div className="chrome-x"><i className="material-icons" onClick={this.close}>close</i></div>
-                            <div className="chrome-btn"><i className="material-icons" onClick={this.maximize}>{this.state.fullscreenIcon}</i></div>
-                            <div className="chrome-btn"><i className="material-icons">minimize</i></div>
-                        </div>
-                        <div style={{overflow: 'scroll'}}>
-                            {this.renderContent(this.props.target)}
-                        </div>
+  }
+  getGitUrl = () => {
+    let project = projects.filter(
+      (project) => project.shortName === this.props.target
+    );
+    if (!!project)
+      project = otherProjects.filter(
+        (project) => project.shortName === this.props.target
+      );
+    return project[0].url;
+  };
+  render() {
+    let showReadMe = true;
+    let showCode = true;
+    if (this.props.target === "Contact Me" || this.props.target === "Documents")
+      showCode = false;
+    if (projects.filter((x) => x.shortName === this.props.target).length === 0)
+      showReadMe = false;
+    if (!this.props.show) return null;
+    else if (!this.state.fullscreen) {
+      return (
+        <ReactResizeDetector
+          handleWidth
+          handleHeight
+          onResize={this.handleResize}
+        >
+          <Draggable
+            handle=".handle"
+            bounds="parent"
+            defaultPosition={{ x: 0, y: 0 }}
+            position={null}
+            grid={[1, 1]}
+            scale={1}
+          >
+            <div
+              className="chrome-box"
+              style={{ zIndex: this.props.z }}
+              onClick={this.bringToFront}
+            >
+              <div
+                id="handle"
+                className="handle"
+                style={{ cursor: this.state.grabbingCursor }}
+                onMouseDown={this.mouseDown}
+                onMouseUp={this.mouseUp}
+              >
+                <h3>{this.props.target}</h3>
+                <div className="chrome-x">
+                  <i className="material-icons" onClick={this.close}>
+                    close
+                  </i>
                 </div>
-            )
-        }
+                <div className="chrome-btn">
+                  <i className="material-icons" onClick={this.maximize}>
+                    {this.state.fullscreenIcon}
+                  </i>
+                </div>
+                <div className="chrome-btn">
+                  <i className="material-icons" onClick={this.minimize}>
+                    minimize
+                  </i>
+                </div>
+                ;
+                {showReadMe ? (
+                  <div
+                    className="chrome-btn read-me"
+                    onClick={this.toggleReadme}
+                  >
+                    {this.state.readMe ? "Close Read Me" : "Read Me"}
+                  </div>
+                ) : null}
+                {showCode ? (
+                  <div
+                    className="chrome-btn read-me"
+                    style={{ marginRight: "5px" }}
+                    onClick={() => window.open(this.getGitUrl())}
+                  >
+                    View Source
+                  </div>
+                ) : null}
+              </div>
+              <div style={{ overflow: "auto" }}>
+                {this.renderContent(this.props.target)}
+              </div>
+            </div>
+          </Draggable>
+        </ReactResizeDetector>
+      );
+    } else {
+      return (
+        <div style={{ zIndex: 999 }}>
+          <div id="handle" className="handle" style={{ cursor: "auto" }}>
+            <h3>{this.props.target}</h3>
+            <div className="chrome-x">
+              <i className="material-icons" onClick={this.close}>
+                close
+              </i>
+            </div>
+            <div className="chrome-btn">
+              <i className="material-icons" onClick={this.maximize}>
+                {this.state.fullscreenIcon}
+              </i>
+            </div>
+            <div className="chrome-btn">
+              <i className="material-icons">minimize</i>
+            </div>
+          </div>
+          <div style={{ overflow: "scroll" }}>
+            {this.renderContent(this.props.target)}
+          </div>
+        </div>
+      );
     }
+  }
 }
--- src/Global Components/ShowProject.js
+++ src/Global Components/ShowProject.js
@@ -1,63 +1,78 @@
-import React, { Component } from 'react'
-import data from '../other-projects.json';
-import { Carousel } from 'react-responsive-carousel';
+import React, { Component } from "react";
+import data from "../other-projects.json";
+import projects from "../projects.json";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default class ShowProject extends Component {
-    constructor(props) {
-        super(props);
-        this.project = data.filter(x => x.name === this.props.name)[0];
-        this.renderMD = this.renderMD.bind(this);
+  constructor(props) {
+    super(props);
+    this.project = data.filter((x) => x.name === this.props.name)[0];
+    if (!this.project) {
+      this.project = projects.filter((x) => x.name === this.props.name)[0];
     }
-    renderMD(){
-        const data = this.project.info.split('\n');
-        let res = data.map(element => {
-            if(element.startsWith('####')){
-                element = <h4>{element.replace('####','')}</h4>;
-            }
-            else if(element.startsWith('###')){
-                element = <h3>{element.replace('###','')}</h3>;
-            }
-            else if(element.startsWith('##')){
-                element = <h2>{element.replace('##','')}</h2>;
-            }
-            else if(element.startsWith('#')){
-                element = <h1>{element.replace('#','')}</h1>;
-            }
-            else if(element.startsWith('-')){
-                element = <ul><li>{element.replace('-','')}</li></ul>
-            }
-            return <p>{element}</p>
-        })
-        return res;
+    this.renderMD = this.renderMD.bind(this);
+  }
+  renderMD() {
+    const data = this.project.info.split("\n");
+    let res = data.map((element) => {
+      if (element.startsWith("####")) {
+        element = <h4>{element.replace("####", "")}</h4>;
+      } else if (element.startsWith("###")) {
+        element = <h3>{element.replace("###", "")}</h3>;
+      } else if (element.startsWith("##")) {
+        element = <h2>{element.replace("##", "")}</h2>;
+      } else if (element.startsWith("#")) {
+        element = <h1>{element.replace("#", "")}</h1>;
+      } else if (element.startsWith("-")) {
+        element = (
+          <ul>
+            <li>{element.replace("-", "")}</li>
+          </ul>
+        );
+      }
+      return <p>{element}</p>;
+    });
+    return res;
+  }
+  renderImages() {
+    if (this.project.image.length === 1) {
+      return (
+        <img
+          src={this.project.image}
+          style={{ width: this.props.width - 100 }}
+        />
+      );
+    } else {
+      return (
+        <div className="image-carousel">
+          <Carousel
+            autoPlay
+            showThumbs={false}
+            infiniteLoop={true}
+            dynamicHeight={true}
+          >
+            {this.project.image.map((element) => (
+              <div style={{ paddingRight: "2%" }}>
+                <img src={element} />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      );
     }
-    renderImages(){
-        if(this.project.image.length === 1){
-            return (<img src={this.project.image} style={{width: this.props.width - 100}} />);
-        }
-        else{
-            return (
-                <div className="image-carousel">
-                    <Carousel autoPlay showThumbs={false} infiniteLoop={true} dynamicHeight={true}>
-                        {this.project.image.map((element) =>(
-                            <div style={{paddingRight: '2%'}}>
-                                <img src={element} />
-                            </div>
-                        ))}
-                    </Carousel>
-                </div>  
-            )
-        }
-    }
-    render() {
-        return (
-            <div className="project-container noselect" style={{height: this.props.height - 40, width: this.props.width}}>
-                <h1>{this.project.name}</h1>
-                <button onClick={() => {window.open(this.project.repo, "_blank")}}>View Code</button>
-                <h3>Completed project:</h3>
-                {this.renderImages()}
-                <h3>Project info:</h3>
-                <div className="text-container">{this.renderMD()}</div>
-            </div>
-        )
-    }
+  }
+  render() {
+    return (
+      <div
+        className="project-container noselect"
+        style={{ height: this.props.height - 40, width: this.props.width }}
+      >
+        <h1>{this.project.name}</h1>
+        {this.project.image.length > 0 ? <h3>Completed project:</h3> : null}
+        {this.renderImages()}
+        <h3>Project info:</h3>
+        <div className="text-container">{this.renderMD()}</div>
+      </div>
+    );
+  }
 }
