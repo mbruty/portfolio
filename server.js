a689a0d035a1aff344e45de3b6e0c6390339ac67
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-10-14 14:02:30 +0100

    Added a console log!

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+console.log(process.env);
+
 const emailTransporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
